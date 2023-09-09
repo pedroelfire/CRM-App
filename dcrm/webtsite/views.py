ffdad04f5eb6cfe6c71c3093819f140863272bae
pedroelfire <pedroelfire@gmail.com>
2023-09-08 21:28:02 -0600

    Listooo

--- dcrm/webtsite/views.py
+++ dcrm/webtsite/views.py
@@ -22,4 +22,9 @@ def home(request):
         return render(request, 'home.html', {})
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "You have been logged out...")
+    return redirect('home')
+
+def register_user(request):
+    return render(request, 'register.html', {})
